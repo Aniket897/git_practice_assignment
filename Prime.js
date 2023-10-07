cc4763e95925ad66cad76383adf113b66e443a8b
Bhushan <bm6146@gmail.com>
2023-10-07 23:20:34 +0530

    prime js fix

--- Prime.js
+++ Prime.js
@@ -1,9 +1,11 @@
 function CheckPrime(number){
     let count = 0;
-    for(let i = 1; i <= number; i++){
+    for(let i = 0; i <= number; i++){
         if(number % i == 0){
             count++;
         }
     }
-    return count == 2;
+    console.log(count);
 }
+
+CheckPrime(22);
